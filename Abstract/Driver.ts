ac83a0e3a2e09284bc6f6119406c03cd7b9a760a
Anchal Singh <anchals0915@gmail.com>
2024-03-27 19:10:52 +0530

    Script to demonstrate classes

---
+++ Abstract/Driver.ts
@@ -0,0 +1,19 @@
+import { Shape } from "./Shape";
+import { Circle } from "./Circle";
+import { Rectangle } from "./Rectangle";
+
+// Create instances of Shape, Circle, and Rectangle
+let myCircle = new Circle(3, 6, 9);
+let myRectangle = new Rectangle(2, 4, 6, 8);
+
+// Store instances in an array of Shape objects
+let theShapes: Shape[] = [];
+theShapes.push(myCircle);
+theShapes.push(myRectangle);
+
+console.log("Shapes:");
+for (let eachShape of theShapes) {
+    console.log(eachShape.getInfo());
+    console.log("Area = " + eachShape.calculateArea());
+    console.log();
+}

5e7496db45fa412bff82bf660f617c2375f66597
Anchal Singh <anchals0915@gmail.com>
2024-03-28 19:11:36 +0530

    generated js files

---
+++ Interface/CricketCoach.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.CricketCoach = void 0;
+class CricketCoach {
+    getDailyWorkout() {
+        return "Pratice atleast 1000 bowls daily !!";
+    }
+}
+exports.CricketCoach = CricketCoach;
